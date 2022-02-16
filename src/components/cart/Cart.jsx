1ba67c4e845ebc96187d72fef1253e861af74fad
Pedro Cazalá <cazalapedro@gmail.com>
2022-02-16 16:31:32 -0300

    cambio de un texto

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -78,7 +78,7 @@ function Cart() {
         }else{terminarCompra()}
     }
     const acceptOrderSummary=()=>{
-        alert('sisi claro que si')
+        alert('Cambiar el seteo, para que desapareca el resumen')
     }
     return (
         <div>
